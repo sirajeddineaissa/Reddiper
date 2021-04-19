c7193f5b4099d1f24ea6700e91fd97be59b0c5e7
Dismalness <domainexpansion@protonmail.com>
2021-04-19 20:55:00 +0100

    fixed writing ignored images

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import os
 import pickle
 
 #Amount of posts to scrape
-quantity = 5
+quantity = 20
 
 def create_token():
     creds = {}
@@ -22,6 +22,7 @@ def create_folder(image_path):
     if not CHECK_FOLDER:
         os.makedirs(image_path)
 
+bypass = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bypass/")
 image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images/")
 create_folder(image_path)
 if os.path.exists('token.pickle'):
@@ -46,17 +47,28 @@ for item in list:
     subimage_path = os.path.join(image_path, f"{sub}")
     create_folder(subimage_path)
     print(f"Scraping from {sub}!")
-    count = 0
     for submission in subreddit.new(limit=quantity):
         if "jpg" in submission.url.lower() or "png" in submission.url.lower():
             try:
                 resp = requests.get(submission.url.lower(), stream=True).raw
-                image = np.asarray(bytearray(resp.read()), dtype="uint8")
-                image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-                cv2.imwrite(os.path.join(subimage_path , f"{submission.id}.png"), image)
-                count += 1
-                    
+                image = cv2.imdecode(np.asarray(bytearray(resp.read()), dtype="uint8"), cv2.IMREAD_COLOR)
+                compare_image = cv2.resize(image,(224,224))
+                for (dirpath, dirnames, filenames) in os.walk(bypass):
+                    bypass_path = [os.path.join(dirpath, file) for file in filenames]
+                ignored = False
+
+                for ignore in bypass_path:
+                    diff = cv2.subtract(cv2.imread(ignore), compare_image)
+                    b, g, r = cv2.split(diff)
+                    res = cv2.countNonZero(b) + cv2.countNonZero(g) + cv2.countNonZero(r)
+                    if res == 0:
+                        ignored = True
+
+                if not ignored:
+                    cv2.imwrite(os.path.join(subimage_path , f"{submission.id}.png"), image)
+
             except Exception as e:
                 print(f"Error!! {submission.url.lower()}")
                 print(e)
         
+
